eec109bfc40da6b7f5dd4b4fd46521f577cbbadb
Jatin Singh <sagoisinghjatin9951@gmail.com>
2022-12-27 01:54:58 +0530

    text

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "./components/header";
-import Meme from "./components/meme"
+import Meme from "./components/meme";
 
 export default function App() {
   return (
     <div>
       <Header />
-      <Meme/>
+      <Meme />
     </div>
   );
 }
--- src/components/meme.jsx
+++ src/components/meme.jsx
@@ -16,20 +16,46 @@ export default function Meme() {
     // console.log(url);
     setMeme((prevMeme) => ({
       ...prevMeme,
-      image:url,
+      image: url,
+    }));
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
     }));
   }
 
   return (
     <main>
       <div className="form">
-        <input type="text" className="form--input" placeholder="Top Text" />
-        <input type="text" className="form--input" placeholder="Bottom Text" />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Top Text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Bottom Text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button className="form--btn" onClick={getNewMeme}>
           Get a new image
         </button>
       </div>
-      <img src={meme.image} className="meme--image" />
+      <div className="meme">
+        <img src={meme.image} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
